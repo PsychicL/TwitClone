bb056d31c5b83b907562bb6ecbf10d7070c639f8
PsychicL <leovic.flak00@gmail.com>
2023-01-03 00:31:17 +0100

    fixed db

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from markupsafe import escape
+from sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
