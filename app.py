e5f487757eee7bd2bf81fe843717705ac9e10bc8
PsychicL <leovic.flak00@gmail.com>
2023-01-08 12:12:36 +0100

    sign up/log in implemented

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
-from models.User import User
 from datetime import datetime
+from flask import session
 
 
 
@@ -11,12 +11,13 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+app.secret_key = b'sSA!)-.-)_+<%!!-_+*@w21R'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, nullable=False)
-    pwd = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
 @app.route("/")
@@ -35,13 +36,27 @@ def signup():
         user = User(
             username=request.form["username"],
             email=request.form["email"],
-            pwd=request.form["password"]
+            password=request.form["password"]
         )
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("index", id=user.id))
 
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        user = User.query.filter_by(username=request.form["username"], password=request.form["password"]).first()
+        if user:
+            session['id'] = user.id
+            return redirect(url_for("index"))
+        else:
+            return redirect("/loginerror")
         
+@app.route("/loginerror")
+def loginerror():
+    return render_template("loginerror.html")
 
 db.init_app(app)
 if __name__ == "__main__":
